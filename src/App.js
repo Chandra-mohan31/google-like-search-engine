7f1b5c5998f17965c30b59ac0d694dfba2e9e4f9
Chandra Mohan.S <chandrashanmugam007@gmail.com>
2020-08-24 18:53:19 +0530

    started with building a googlr like search engine using react

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Home from "./pages/Home";
+import {BrowserRouter as Router ,Switch,Route } from "react-router-dom";
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>this is the search page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    
+   
+     
+    </div>
+  );
+}
+
+export default App;
